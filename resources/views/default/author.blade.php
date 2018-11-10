6d5b91cceca71ffee4557c3f1d334c91c29056f9
xf <759538105@qq.com>
2018-11-10 19:29:12 +0800

    修改显示微信

--- resources/views/default/author.blade.php
+++ resources/views/default/author.blade.php
@@ -10,15 +10,15 @@
         $qq = '';
         if(!isset($user->id)||$user->id == 1){
             $github_url = $systemPresenter->getKeyValue('github_url');
-            $qq = $systemPresenter->getKeyValue('qq');
+            $weixin = $systemPresenter->getKeyValue('weixin');
         }
         ?>
         <li class="wx"><img src="{{ asset('uploads/avatar')."/".$author->user_pic }}"></li>
         @if ($github_url != "")
          <li class="tencent"><a href="{{ $github_url }}" target="_blank"><span>GitHub</span>Finley</a></li>
          @endif
-         @if($qq != "")
-        <li class="qq"><a><span>QQ号</span>{{ $qq }}</a></li>
+         @if($weixin != "")
+        <li class="qq"><a><span>微信号</span>{{ $weixin }}</a></li>
         @endif
     </ul>
 </div>
